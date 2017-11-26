50462a2bccf4638a95136160c02585a314f01e69
Denis <Nolane16@gmail.com>
2017-11-26 17:23:03 +0300

    Fix type of texture coordinates

--- AffineTransformationsIn3D/Geometry/MeshWithTexture.cs
+++ AffineTransformationsIn3D/Geometry/MeshWithTexture.cs
@@ -7,7 +7,7 @@ namespace AffineTransformationsIn3D.Geometry
         Bitmap texture;
         PointF[] textureCoordinates;
 
-        public MeshWithTexture(Vector[] vertices, int[][] indices, PointF[][] textureCoordinates) : base(vertices, indices)
+        public MeshWithTexture(Vector[] vertices, int[][] indices, PointF[] textureCoordinates) : base(vertices, indices)
         {
             this.textureCoordinates = textureCoordinates;
         }
